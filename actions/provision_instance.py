66ed1ef051ce90855bb0e4f85753af5b1a376f4c
Martez Reed <martez.reed@greenreedtech.com>
2022-02-27 09:55:18 -0600

    Add additional actions

---
+++ actions/provision_instance.py
@@ -0,0 +1,9 @@
+payload = {}
+payload['zoneId'] = 1
+payload['instance'] = {}
+payload['config'] = {}
+payload['config']['resourcePoolId'] = 1
+
+payload['config']['createUser'] = True
+payload['volumes'] = []
+payload['networkInterfaces'] = []
